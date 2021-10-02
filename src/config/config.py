29a5d23438f25abc4cf0560e574256cbd0e88f53
N-z0 <syslog@laposte.net>
2021-10-02 08:27:20 +0700

    upgrade config module
    Previously the configuration format and defaults was hard-coded.
    Now an YML file can be used.

--- src/config/config.py
+++ src/config/config.py
@@ -7,7 +7,7 @@
 
 __doc__ = "this modul is useful for setting programs at startup, returning the merge of configuration files and command line instructions"
 __status__ = "Development"#should be one of 'Prototype' 'Development' 'Production' 'Deprecated' 'Release'
-__version__ = "v3.0.0"# version number,date or about last modification made compared to the previous version
+__version__ = "v4.0.0"# version number,date or about last modification made compared to the previous version
 __license__ = "public domain"# ref to an official existing License
 __date__ = "2008/10"#started creation date / year month day
 __author__ = "N-zo syslog@laposte.net"#the creator origin of this prog,
@@ -39,32 +39,104 @@ import string # manipulation of character strings
 import shlex # Split strings using shell-like syntax, ignoring comment in the string .
 from distutils import util # strtobool(string) convert	true y yes true on 1		false n no f off 0	Raises ValueError if anything else
 
+#import yaml # This imports PyYAML. Stop doing this.
+from ruamel import yaml # This imports "ruamel.yaml". Always do this.
+### Always use ruamel.yaml. Never use PyYAML.
+### PyYAML is a fetid corpse rotting in the mouldering charnel ground of PyPi.
 
 
-CFG_ERROR_MSG1="config files error: missing section [{}]"
-CFG_ERROR_MSG2="config files error: missing '{}' in section [{}]"
+
+#CFG_ERROR_MSG1="config files error: missing section [{}]"
+#CFG_ERROR_MSG2="config files error: missing '{}' in section [{}]"
 CFG_ERROR_MSG3="config files error: invalid value for '{}' in section [{}]"
 CMD_INDEX_SEPARATOR='='
 CMD_CAT_ARG="ARG" # section for the given args 
 CMD_PIPE_ARG='-'# use for a specifie input or output args by pipe
 #CMD_VERSION_MSG="show program version" #prefer to restrict cmd help to how to use
 DESCRIPTION=None # use by the comandline --help for additional information,should keep this text common generic
+NAME='name'
+CATEGORY='category'
+HELP='help'
+SHORTFLAG='shortflag'
+LONGFLAG='longflag'
+TYPE='type'
+QANTUM='qantum'
+CHOICES='choices'
+DEFAULT='default'
+SYMBOLS='symbols'
+CATEGORY_ARGUMENT="argument"
+CATEGORY_CHOICE="choice"
+CATEGORY_VALU="valu"
+CATEGORY_NEGATIV_FLAG="negativ_flag"
+CATEGORY_POSITIV_FLAG="positiv_flag"
 
 
 
 class Config :
 	"""for the merge of command lines and configuration files"""
 	def __init__(self):
+		self.default={CMD_CAT_ARG:{}}
 		self.cfg=Config_File()
 		self.cmd=Command_line()
 
-	def read_configfile(self,pathname):
-		"""parse the primary default initfiles"""
-		self.cfg.read_configfile(pathname)
-		
-	def read_optional_configfiles(self,init_file_list):
-		"""parse the optional initfiles, listed by priority order"""
-		self.cfg.read_optional_configfiles(init_file_list)
+	def read_format(self,yml_file):
+		"""get format and defaults from yml file"""
+		tipes={"str":str,"int":int,"bool":bool,"float":float}
+		### many ymlloading modes are available:
+		### RoundTripLoader allow to keep comments but the types of returned data are not regular python data, ex: ruamel.yaml.scalarfloat.ScalarFloat ruamel.yaml.comments.CommentedSeq
+		#data = yaml.round_trip_load(open(yml_file,'r'))
+		### safe_load allow to keep regular python data type
+		data = yaml.safe_load(open(yml_file,'r'))
+		### 'load(stream)' without further arguments can be unsafe. Use 'load(stream, Loader=ruamel.yaml.Loader)'
+		#data = yaml.load(open(yml_file,'r'))
+		### load_all produce error warning: you should consider using safe_load(stream)
+		#data = yaml.load_all(open(yml_file,'r'))
+		for section in data:
+			if not section in self.default :
+				self.default[section]={}
+			for parameter in data[section] :
+				name=parameter.get(NAME)
+				category=parameter.get(CATEGORY)
+				help=parameter.get(HELP)
+				shortflag=parameter.get(SHORTFLAG)
+				if shortflag :
+					shortflag="-"+shortflag
+				longflag=parameter.get(LONGFLAG)
+				if longflag :
+					longflag="--"+longflag
+				tip=tipes.get( parameter.get(TYPE) )
+				qantum=parameter.get(QANTUM)
+				choices=parameter.get(CHOICES)
+				if choices==None :
+					pass
+				else :
+					choices= tuple(map(tip,choices))
+				default=parameter.get(DEFAULT)
+				if default==None :
+					pass
+				elif type(default)==list :
+					default= tuple(map(tip,default))
+				else :
+					default= tip(default)
+				symbols=parameter.get(SYMBOLS)
+				#print(name,category,tip,help,qantum,default,shortflag,longflag,help)
+				if category==CATEGORY_ARGUMENT :
+					self.add_arg(name,tip,qantum,help)
+				elif category==CATEGORY_CHOICE :
+					self.default[section][name]=default
+					self.add_choice(shortflag,longflag,tip,qantum,choices,help,section,name)
+				elif category==CATEGORY_VALU :
+					self.default[section][name]=default
+					self.add_valu(shortflag,longflag,tip,qantum,help,symbols,section,name)
+				elif category==CATEGORY_NEGATIV_FLAG :
+					self.default[section][name]=True
+					self.add_negative_flag(shortflag,longflag,help,section,name)
+				elif category==CATEGORY_POSITIV_FLAG :
+					self.default[section][name]=False
+					self.add_positive_flag(shortflag,longflag,help,section,name)
+				else :
+					pass
+		#print(self.default)
 
 	def add_arg(self,arg,tip,qantum,help):
 		"""request for one or more values ​​in the command line
@@ -93,19 +165,21 @@ class Config :
 		self.cmd.add_valu(shortflag,longflag,tip,qantum,help,symbols,category,name)
 		self.cfg.add_format(category,name,tip,qantum,None)
 
+	def read_configfiles(self,init_file_list):
+		"""parse the optional initfiles, listed by priority order"""
+		self.cfg.read_optional_configfiles(init_file_list)
+		
 	def get(self):
 		"""merge and return the configuration files and the command line parameters"""
-
-		setting=self.cfg.get()
-		#print(cfg)
-		mode=self.cmd.get()
-		#print(cmd)
-
-		for category in mode.keys() :
-			if not category in setting :
-				setting[category]={}
-			setting[category].update(mode[category])
-
+		setting=self.default
+		cfg=self.cfg.get()
+		cmd=self.cmd.get()
+		for category in setting.keys() :
+			if category in cfg :
+				setting[category].update(cfg[category])
+			if category in cmd :
+				setting[category].update(cmd[category])
+		#print(setting)
 		return setting
 
 
@@ -135,18 +209,22 @@ class Config_File :
 	def get(self):
 		"""return result of the parsed configuration files settings"""
 		cfg={ }
-				
+		
 		for category in self.format :
 			for name in self.format[category] :
 				data_format=self.format[category][name]
 				try:
 					data=self.parser.get(category,name)
 				except configparser.NoSectionError :
+					### by giving default and format, sections in conf files become not required
+					pass
 					### try to have the same argparse error behavior
-					sys.exit(CFG_ERROR_MSG1.format(category))# write error on stderr					
+					#sys.exit(CFG_ERROR_MSG1.format(category))# write error on stderr
 				except configparser.NoOptionError :
+					### by giving default and format, options in conf files become not required
+					pass
 					### try to have the same argparse error behavior
-					sys.exit(CFG_ERROR_MSG2.format(name,category))# write error on stderr	
+					#sys.exit(CFG_ERROR_MSG2.format(name,category))# write error on stderr	
 				else:
 					#print( "[{}]{} {}".format(category,name,data) )
 					data_list=shlex.split(data)
@@ -155,7 +233,7 @@ class Config_File :
 					if ( data_qantum!=data_format["qantum"] and data_format["qantum"]!=0 ) or data_qantum==0 :
 						### try to have the same argparse error behavior
 						sys.exit(CFG_ERROR_MSG3.format(name,category))# write error on stderr	
-
+						
 					### config parser also provide getboolean() getint() and getfloat()
 					### but it's not convenient to check format type and then using the matching parser method.
 					for index in range(data_qantum) :
@@ -177,17 +255,17 @@ class Config_File :
 						if data_format["choices"] and not data in data_format["choices"] :
 							### try to have the same argparse error behavior
 							sys.exit(CFG_ERROR_MSG3.format(name,category))# write error on stderr	
-
+						
 						data_list[index]=data
 						
-					else: # Executed if no break in for 
+					else: # Executed if no break in previous for loop
 						if not category in cfg :
 							cfg[category]={}
 						if data_qantum==1 :
 							cfg[category][name]=data_list[0]
 						else :
 							cfg[category][name]=data_list
-				
+		#print(cfg)
 		return cfg
 
 
@@ -197,8 +275,9 @@ class Command_line :
 	def __init__(self):
 		#print(sys.argv)# print the full command line
 		self.parser =  argparse.ArgumentParser(description=DESCRIPTION)#,allow_abbrev=False)
+		### remove the ability to get program version because tedious to maintain and prone to mistakes
 		#self.parser.add_argument('-v','--version',action='version',version=name+version,help=CMD_VERSION_MSG)
-
+		
 	def add_arg(self,arg,tip,qantum,help):
 		"""specifies the request for one or more values ​​in the command line"""
 		if qantum==0 : qantum='+' #convert to argparse nargs
@@ -209,23 +288,23 @@ class Command_line :
 		index=category+CMD_INDEX_SEPARATOR+name
 		if qantum==0 : qantum='+' #convert to argparse nargs
 		self.parser.add_argument(shortflag,longflag,type=tip,nargs=qantum,choices=choices,help=help,dest=index)
-
+		
 	def add_positive_flag(self,shortflag,longflag,help,category,name):
 		"""for these flags in the command line a True value will be stored"""
 		index=category+CMD_INDEX_SEPARATOR+name
 		self.parser.add_argument(shortflag,longflag,action='store_const',const=True,help=help,dest=index)
-
+		
 	def add_negative_flag(self,shortflag,longflag,help,category,name):
 		"""for these flags in the command line a False value will be stored"""
 		index=category+CMD_INDEX_SEPARATOR+name
 		self.parser.add_argument(shortflag,longflag,action='store_const',const=False,help=help,dest=index)
-
+		
 	def add_valu(self,shortflag,longflag,tip,qantum,help,symbols,category,name):# tip can be : bool float int string any*
 		"""for these flags in the command line one or many optional values will be stored"""
 		index=category+CMD_INDEX_SEPARATOR+name
 		if qantum==0 : qantum='+' #convert to argparse nargs
 		self.parser.add_argument(shortflag,longflag,type=tip,nargs=qantum,help=help,metavar=symbols,dest=index)
-
+		
 	def get(self):
 		"""return result of the parsed command line settings"""
 		cmd={ }
@@ -234,7 +313,7 @@ class Command_line :
 		for index in args.keys() :
 			data=args[index]
 			if data is not None :
-				#print(index,data,type(data))#
+				#print(index,data,type(data))
 				index=index.split(CMD_INDEX_SEPARATOR)
 				if len(index)>1 :
 					cat=index[0]
